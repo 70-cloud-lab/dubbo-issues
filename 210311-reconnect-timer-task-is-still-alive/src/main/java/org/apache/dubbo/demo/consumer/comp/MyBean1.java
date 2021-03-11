87b3c924a869546432e4bfece161221921929b45
zhangyz-hd <hotdonkey@163.com>
2021-03-11 19:15:05 +0800

    add issue demo 210311

---
+++ 210311-reconnect-timer-task-is-still-alive/src/main/java/org/apache/dubbo/demo/consumer/comp/MyBean1.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.demo.consumer.comp;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.demo.DemoServiceA1;
+import org.springframework.stereotype.Component;
+
+@Component("myBean1")
+public class MyBean1 {
+    @Reference(version = "2.0", check = false, parameters = {"a", "1"})
+    private DemoServiceA1 a1;
+
+    public void test() {
+        while (true) {
+            try {
+                System.in.read();
+                System.out.print(a1.sayHello("123"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
