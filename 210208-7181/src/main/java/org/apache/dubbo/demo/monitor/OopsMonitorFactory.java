42b3a09de6639e2a88e2d1c9648508fe817e49a1
zhangyz-hd <zhangyzhd@163.com>
2021-03-29 10:09:21 +0800

    update 7181

--- 210208-7181/src/main/java/org/apache/dubbo/demo/monitor/OopsMonitorFactory.java
+++ 210208-7181/src/main/java/org/apache/dubbo/demo/monitor/OopsMonitorFactory.java
@@ -7,6 +7,7 @@ import org.apache.dubbo.monitor.support.AbstractMonitorFactory;
 public class OopsMonitorFactory extends AbstractMonitorFactory {
     @Override
     protected Monitor createMonitor(URL url) {
+        System.out.println("NEW OOPS");
         return new OopsMonitor(url);
     }
 }
